394d9dbd1500b0c3aa06f125bca3e4986439deb2
Matt Wiseman <mrslwiseman@gmail.com>
2017-11-24 12:02:30 +1100

    modify index route and add routes comments

--- app.js
+++ app.js
@@ -26,16 +26,47 @@ passport.deserializeUser(User.deserializeUser());
 app.use(passport.initialize());
 app.use(passport.session());
 
-// ROUTES
-
+// RESTful ROUTES
+// index route
 app.get("/", (req,res) => {
+  res.redirect("/members")
+})
 
-res.render("home", {req:req});
+app.get("/members", (req,res) => {
+  res.render("index", {req: req, members: members})
 })
 
 app.get("/dashboard", isLoggedIn, (req,res) => {
   res.render("dashboard", {req:req})
 })
+
+// New route
+// app.get /recipes/new
+// render new member signup form
+
+// Create route
+// app.post /recipes
+// add member to db
+// redirect to index route
+
+// Show route
+// app.get /recipes/:id
+// render member profile view (this might not be needed)
+
+// Edit route
+// app.get /member/:id/edit
+// Render edit view with member object
+
+// Update route
+// app.put /recipes/:id
+// find and update
+// redirect to member page
+
+// Destroy route
+// app.delete /blogs/:id 
+// find by id and delete
+// redirect to index
+
 
 // AUTH ROUTES
 
