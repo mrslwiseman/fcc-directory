c59b903fa60b2081b7e3944a323948fec47ae37e
Matt Wiseman <mrslwiseman@gmail.com>
2017-11-24 18:17:03 +1100

    remove semi colon

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,5 +1,5 @@
 const fs = require('fs'),
-      path = require("path");
+      path = require("path"),
       membersData = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '/data/dummy-data-members.json'), 'utf-8')); // remove this when hoooked up to db
 
 
