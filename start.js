9f56527c2031516fb0a5c26e4dbc2f896accdd5f
Matt Wiseman <mrslwiseman@gmail.com>
2017-11-24 12:05:42 +1100

    disable db connection

--- start.js
+++ start.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
 // import environmental variables from our variables.env file
-require('dotenv').config({ path: 'variables.env' });
+// require('dotenv').config({ path: 'variables.env' });
 
 // Connect to our Database and handle an bad connections
-mongoose.connect(process.env.DATABASE);
-mongoose.Promise = global.Promise;
-mongoose.connection.on('error', (err) => {
-  console.error(`${err.message}`);
-});
+// mongoose.connect(process.env.DATABASE);
+// mongoose.Promise = global.Promise;
+// mongoose.connection.on('error', (err) => {
+//   console.error(`${err.message}`);
+// });
 
 const app = require('./app');
-app.set('port', process.env.PORT);
+app.set('port', process.env.PORT || 3000);
 
 const server = app.listen(app.get('port'), function() {
     console.log('App is running on port', app.get('port'));
