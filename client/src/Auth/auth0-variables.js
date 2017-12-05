3043cee7b1271c950de27b6e6a3517d749800c4c
Matt Wiseman <mrslwiseman@gmail.com>
2017-12-05 21:47:25 +1100

    modify callback

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
     domain: 'chucknorrisworld.au.auth0.com',
     clientId: 'tRMRS8h4W6eUiNMv9LUv5NkMHHPToIxd',
-    callbackUrl: 'http://localhost:3000/callback'
+    callbackUrl: 'https://immense-forest-46365.herokuapp.com/callback'
   }
   
