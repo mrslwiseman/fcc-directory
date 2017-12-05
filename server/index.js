7ca930d6d6bfe3a287e0ee8b1690c8f6ec53a9ed
Matt Wiseman <mrslwiseman@gmail.com>
2017-12-05 17:22:53 +1100

    try to get heroku up

--- server/index.js
+++ server/index.js
@@ -6,21 +6,27 @@ const express        = require('express'),
       app            = express();
       runScript      = require('./data/populate_db');
       
-require('dotenv').config({ path: 'variables.env' });
-
+require('dotenv').config({ path: path.resolve(__dirname, '../variables.env') });
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.use(bodyParser.json());       // to support JSON-encoded bodies, which axios does by default
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost/fcc-directory')
+const database_mode = 
+  process.env.NODE_ENV == 'production' 
+  ? process.env.DATABASE_PROD 
+  : process.env.DATABASE_DEV
+
+mongoose.connect(database_mode)
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
   // we're connected!
-  console.log('connected to db....')
-  runScript.populateDB();
+  console.log('connected to db on ' + database_mode )
+  if(process.env.NODE_ENV != 'production' ){
+    runScript.populateDB();
+  }
 });
 
 
