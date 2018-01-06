8ada5924e2182516bf5fe6f3e72307ead8afc680
Matt Wiseman <mrslwiseman@gmail.com>
2018-01-06 18:40:39 +1100

    restructure Member component with props

--- client/src/components/Member.js
+++ client/src/components/Member.js
@@ -1,40 +1,39 @@
 import React, { Component } from 'react';
 
-export default class Member extends Component {
-    render() {
-        return (
-            <div className="m">
-                    <div className="m__photo">
-                        <img src="./images/image.jpg" className="m__img"/>
-                    </div>
-                    <div className="m__details">
-                        <div className="m__name">
-                            <h2>Matt Wiseman</h2>
-                        </div>
-                        <div className="m__stack">
-                            <span className="m__stack-item">Mongo,</span>
-                            <span className="m__stack-item">Express,</span>
-                            <span className="m__stack-item">React,</span>
-                            <span className="m__stack-item">Node.js</span>
-                        </div>
-                        <div>
-                            <span className="m__location">Preston</span>
-                        </div>
-                    </div>
-                        <div className="m__connect">
-                            <a className="m__fcc" href="#"></a>
-                            <a className="m__github" href="#"></a>
-                            <a className="m__meetup" href="#"></a>
-                            <a className="m__email" href="#"></a>
-
-                        </div>
-                    <div className="m__bio">
-                        <p>Partial reference works similar to the parent reference, but while parent reference contains the whole selector, partial selectors contain only the first merged N levels of the nested selectors, so you could access those nesting levels individually.</p>
-                    </div>
-                    <div>
-                        <p className="m__joined">Joined April 2017</p>
-                    </div>
+const Member = (props) => {
+    const { chosen_name: first, last_name: last, picture, location, bio, stack, connect, joined } = props
+    return (
+        <div className="m">
+            <div className="m__photo">
+                <img src={picture} className="m__img" />
+            </div>
+            <div className="m__details">
+                <div className="m__name">
+                    <h2>{first} {last}</h2>
                 </div>
-        )
-    }
+                <div>
+                    <p className="m__stack">{stack}</p>
+                </div>
+                <div>
+                    <span className="m__location">{location}</span>
+                </div>
+            </div>
+            <div className="m__connect">
+                {/* map over Object.keys(connect) and generate array of conncetions */}
+                <a className="m__fcc" href={connect.fcc}></a>
+                <a className="m__github" href={connect.github}></a>
+                <a className="m__meetup" href={connect.meetup}></a>
+                <a className="m__email" href={connect.email}></a>
+
+            </div>
+            <div className="m__bio">
+                {bio}
+            </div>
+            <div>
+                <p className="m__joined">Joined {joined}</p>
+            </div>
+        </div>
+    )
 }
+
+export default Member;
