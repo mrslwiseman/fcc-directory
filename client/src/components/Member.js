41121c73e15c7328adb390d1d0e063580c20c71c
Matt Wiseman <mrslwiseman@gmail.com>
2018-01-11 17:29:40 +1100

    add conditional rendering to Member

--- client/src/components/Member.js
+++ client/src/components/Member.js
@@ -4,34 +4,54 @@ const Member = (props) => {
     const { chosen_name: first, last_name: last, picture, location, bio, stack, connect, joined } = props
     return (
         <div className="m">
-            <div className="m__photo">
-                <img src={picture} className="m__img" />
-            </div>
+            {picture &&
+                <div className="m__photo">
+                    <img src={picture} className="m__img" />
+                </div>
+            }
+
             <div className="m__details">
                 <div className="m__name">
                     <h2>{first} {last}</h2>
                 </div>
-                <div>
-                    <p className="m__stack">{stack}</p>
-                </div>
-                <div>
-                    <span className="m__location">{location}</span>
-                </div>
-            </div>
-            <div className="m__connect">
-                {/* map over Object.keys(connect) and generate array of conncetions */}
-                <a className="m__fcc" href={connect.fcc}></a>
-                <a className="m__github" href={connect.github}></a>
-                <a className="m__meetup" href={connect.meetup}></a>
-                <a className="m__email" href={connect.email}></a>
+                {stack &&
+                    <div>
+                        <p className="m__stack">{stack}</p>
+                    </div>
+                }
+
+                {location &&
+                    <div>
+                        <span className="m__location">{location}</span>
+                    </div>
+                }
 
             </div>
-            <div className="m__bio">
-                {bio}
-            </div>
-            <div>
-                <p className="m__joined">Joined {joined}</p>
-            </div>
+
+            {connect &&
+                <div className="m__connect">
+                    {/* map over Object.keys(connect) and generate array of conncetions */}
+                    {
+                        Object.keys(connect) // [ 'email', 'fcc', 'github', 'meetup' ]
+                            .map((connection, i) => {
+                                return <a className={`m__${connection}`} key={i} href={`${connect[connection]}`}></a>
+                            })
+                        }
+                </div>
+            }
+
+            {bio &&
+                <div className="m__bio">
+                    {bio}
+                </div>
+            }
+            {joined &&
+                <div>
+                    <p className="m__joined">Joined {joined}</p>
+                </div>
+            }
+
+
         </div>
     )
 }
